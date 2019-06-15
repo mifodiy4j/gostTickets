bea5270ed5e11421d4d23d8aaf64392a59ae814b
Sergey Mikhailov <mifodiy15@gmail.com>
2019-06-15 08:34:42 +0300

    ticketOne: done

---
+++ src/TicketOne.java
@@ -0,0 +1,27 @@
+public class TicketOne {
+
+    private static final String TWO = "Two";
+    private static final String SEVEN = "Seven";
+    private static final String TWO_SEVEN = "TwoSeven";
+    private static final String SEPARATOR = "; ";
+
+    public void outNumber(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(checkNumber(i));
+            System.out.print(SEPARATOR);
+        }
+    }
+
+    private String checkNumber(int n) {
+        String result = String.valueOf(n);
+        if (n%7 == 0) {
+            result = SEVEN;
+            if (n%2 == 0) {
+                result = TWO_SEVEN;
+            }
+        } else if (n%2 == 0) {
+            result = TWO;
+        }
+        return result;
+    }
+}
---
+++ test/TicketOneTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class TicketOneTest {
+
+    @Test
+    void outNumber() {
+        TicketOne ticketOne = new TicketOne();
+        ticketOne.outNumber(100);
+    }
+}
